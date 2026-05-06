f7cac48f3be19be3157fbe7f5a40ecb799e5a99c
Nurseda <nurseda.karabacak@outlook.com>
2026-05-06 11:44:30 +0300

    Time: 0 ms (100%), Space: 12.9 MB (82.41%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self,nums,target):
+        num_map = {}
+        for i,num in enumerate(nums):
+            diff = target - num 
+            if diff in num_map :
+                return [num_map[diff],i]
+            num_map[num] = i
